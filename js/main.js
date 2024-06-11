20f1226afe8454fab2fa496badee9fb4507d2211
YahyaMohamedMahmoud <151754506+YahyaMohamedMahmoud@users.noreply.github.com>
2024-06-11 20:20:34 +0300

    editcode

--- js/main.js
+++ js/main.js
@@ -59,5 +59,54 @@ new Glider(document.querySelector('.glider'), {
       },
   });
 
+  const swiper2 = new Swiper('.swiper-container2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 'auto', 
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination2',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+  },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next2',
+      prevEl: '.swiper-button-prev2',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar2',
+    },
+    breakpoints: {
+        // when window width is >= 992px (desktop)
+        992: {
+          slidesPerView: 4,
+        },
+        768: {
+            slidesPerView: 2,
+          },
+          600:{
+            slidesPerView: 1,
+          }
+      },
+  });
 
 
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const cards = document.querySelectorAll('.whatCard');
+    const colors = ['#bbf0FF80', '#efeff6', '#f68C201A', '#e6faea', '#efeff6','#f68C201A'];
+
+    cards.forEach((card, index) => {
+        const colorIndex = index % colors.length;
+        card.style.backgroundColor = colors[colorIndex];
+    });
+});
